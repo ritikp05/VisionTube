148360a412736df473a2d73c7d1bfbbdba2e3d7b
Ritik pandey <kushagarakp10@gmail.com>
2024-01-25 20:30:01 +0530

    added .env file

--- src/assets/fetchdata.js
+++ src/assets/fetchdata.js
@@ -7,8 +7,7 @@ const options = {
       maxResults: '50'
     },
     headers: {
-      // 'X-RapidAPI-Key':'5b65811d65mshe4a2b5cb43b53d3p12741cjsn49986bc41eca',
-      'X-RapidAPI-Key': 'dc17cff67dmsh45597ba2856f683p111186jsn59feb92fdbf7',
+      'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
       'X-RapidAPI-Host':'youtube-v31.p.rapidapi.com'
     }
   };
